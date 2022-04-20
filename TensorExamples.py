07f87cd6b5865d71583d893e9624bfe925af0f53
juanpedrorod <juanpedrorodlzma@gmail.com>
2022-04-20 17:06:18 -0500

    Tensorflow tensor examples

---
+++ TensorExamples.py
@@ -0,0 +1,8 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+import tensorflow as tf
+import numpy as np
+
+# This will be an int32 tensor by default; see "dtypes" below.
+rank_0_tensor = tf.constant(4)
+print(rank_0_tensor)

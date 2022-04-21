a038d632af495c46267d3ea3e0830932a1c7ab53
juanpedrorod <juanpedrorodlzma@gmail.com>
2022-04-21 13:27:00 -0500

    segundo com

--- TensorExamples.py
+++ TensorExamples.py
@@ -6,3 +6,8 @@ import numpy as np
 # This will be an int32 tensor by default; see "dtypes" below.
 rank_0_tensor = tf.constant(4)
 print(rank_0_tensor)
+
+rank_1_tensor = tf.constant([1, 2, 3])
+print(rank_1_tensor)
+
+
